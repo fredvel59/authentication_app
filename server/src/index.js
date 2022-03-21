dd9c9911452c26beeda7b2914cf63d2b7ed38566
fredvel59 <fred.vel.dev59@gmail.com>
2022-03-21 12:36:50 -0400

    I added sequelize for postgres database management

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const port = app.get('port');
 
 app.get('/', (req, res) => {
-  res.send({message: 'the server is running successfully'})
+  res.send({message: 'the server is running successfully, now you can navigate to /api, for see more information'})
 } )
 
 app.listen(port, () => {
