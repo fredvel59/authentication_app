961cc156a0167eb1e619c89bf7815dc511e6d78d
fredvel59 <fred.vel.dev59@gmail.com>
2022-03-26 20:34:01 -0400

    I created some endpoint in the client

--- client/src/endPoints/index.js
+++ client/src/endPoints/index.js
@@ -1,7 +1,10 @@
+// docs https://github.com/fredvel59/authentication_app/tree/master/server#readme
+
 const dev = 'http://localhost:8000';
+const production = 'www.example.com';
 
 module.exports = {
   signUp: `${dev}/auth/signup`,
-  
+  logIn: `${dev}/auth/login `
 }
 
